6c86ba975822c419bdf5c7e87022af3036b6eaed
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2024-11-21 21:54:30 +0100

    Setup room database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     ksp(libs.androidx.room.compiler)
     annotationProcessor(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.paging)
 
     implementation(project.dependencies.platform(libs.koin.bom))
     implementation(libs.koin.core)
