ae2d23dbc5575f2bc786c9d8ff004691f57b9c16
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2024-11-24 20:22:45 +0100

    Remove database for now

--- app/src/main/java/cz/frank/spacex/main/data/SpaceXDatabase.kt
+++ app/src/main/java/cz/frank/spacex/main/data/SpaceXDatabase.kt
@@ -3,6 +3,6 @@ package cz.frank.spacex.main.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [], version = 1)
-abstract class SpaceXDatabase : RoomDatabase() {
-}
+//@Database(entities = [], version = 1)
+//abstract class SpaceXDatabase : RoomDatabase() {
+//}
