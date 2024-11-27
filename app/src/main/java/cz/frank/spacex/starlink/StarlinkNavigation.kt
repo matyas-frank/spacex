c10b925c7fb3e20e46bea248eb5207c57301052d
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2024-11-27 10:16:53 +0100

    Better handle current destination in drawer

--- app/src/main/java/cz/frank/spacex/starlink/StarlinkNavigation.kt
+++ app/src/main/java/cz/frank/spacex/starlink/StarlinkNavigation.kt
@@ -1,21 +1,22 @@
 package cz.frank.spacex.starlink
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import cz.frank.spacex.main.ui.NavigationDrawerItem
 import cz.frank.spacex.starlink.ui.map.StarlinkMap
 import kotlinx.serialization.Serializable
 
-@Serializable
 object StarlinkNavigation {
     @Serializable
     data object Map
 }
 
-fun NavGraphBuilder.starlinkNavigation(toggleDrawer: () -> Unit) {
-    navigation<StarlinkNavigation>(StarlinkNavigation.Map) {
+fun NavGraphBuilder.starlinkNavigation(navHostController: NavHostController, toggleDrawer: () -> Unit) {
+    navigation<NavigationDrawerItem.Starlink>(StarlinkNavigation.Map) {
         composable<StarlinkNavigation.Map> {
-            StarlinkMap(toggleDrawer)
+            StarlinkMap(navHostController, toggleDrawer)
         }
     }
 }
