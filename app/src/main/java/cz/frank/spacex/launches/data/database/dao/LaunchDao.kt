5d56461f64e939ed5aee1a0fb9eee6e45f1dc9f1
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2024-12-02 17:31:11 +0100

    Create launch dao features

--- app/src/main/java/cz/frank/spacex/launches/data/database/dao/LaunchDao.kt
+++ app/src/main/java/cz/frank/spacex/launches/data/database/dao/LaunchDao.kt
@@ -1,11 +1,16 @@
 package cz.frank.spacex.launches.data.database.dao
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import cz.frank.spacex.launches.data.database.entity.LaunchEntity
 
 @Dao
 interface LaunchDao {
-    @Insert
-    fun insertAll(vararg launches: LaunchEntity)
+    @Insert fun insertAllLaunches(vararg launches: LaunchEntity)
+
+    @Query("DELETE FROM launch") fun deleteAllLaunches()
+
+    @Query("Select * FROM launch") fun getAllLaunches(): PagingSource<Int, LaunchEntity>
 }
