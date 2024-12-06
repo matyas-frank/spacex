0375d0fa2780586173db78e5537ca68261d53fa6
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2024-12-06 14:58:39 +0100

    Make detail retry one at a time

--- app/src/main/java/cz/frank/spacex/launches/ui/detail/LaunchDetailViewModel.kt
+++ app/src/main/java/cz/frank/spacex/launches/ui/detail/LaunchDetailViewModel.kt
@@ -5,15 +5,15 @@ import androidx.lifecycle.viewModelScope
 import com.fresh.materiallinkpreview.models.OpenGraphMetaData
 import com.fresh.materiallinkpreview.parsing.OpenGraphMetaDataProvider
 import cz.frank.spacex.launches.data.repository.LaunchesRepository
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import okio.IOException
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import java.net.URL
-import kotlin.time.Duration.Companion.seconds
 
 class LaunchDetailViewModel(private val launchId: String) : ViewModel(), KoinComponent {
     private val repository: LaunchesRepository by inject()
@@ -23,16 +23,24 @@ class LaunchDetailViewModel(private val launchId: String) : ViewModel(), KoinCom
     private val _article = MutableStateFlow<Result<OpenGraphMetaData>?>(null)
     val article = _article.asStateFlow()
 
+    private var syncingJob: Job? = null
+    private val syncMutex = Mutex()
+
     init { fetchLaunch() }
 
     fun fetchLaunch() {
         viewModelScope.launch {
-            val res = repository.detailLaunch(launchId)
-            _launch.value =  res
-            res.onSuccess {
-                it.article?.let { _article.value = getLinkMetadata(it) }
+            syncMutex.withLock {
+                if (syncingJob == null) syncingJob = viewModelScope.launch {
+                    val res = repository.detailLaunch(launchId)
+                    _launch.value =  res
+                    res.onSuccess {
+                        it.article?.let { _article.value = getLinkMetadata(it) }
+                    }
+                }
             }
         }
+
     }
 
     private suspend fun getLinkMetadata(link: String): Result<OpenGraphMetaData> {
