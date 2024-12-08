5c5a692184a03beb79706714f008dbe2b56ac159
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2024-12-08 16:19:30 +0100

    Clean up launch api test

--- app/src/test/java/cz/frank/spacex/LaunchApiTest.kt
+++ app/src/test/java/cz/frank/spacex/LaunchApiTest.kt
@@ -5,7 +5,6 @@ import cz.frank.spacex.launches.data.repository.ILaunchesFilterRepository
 import cz.frank.spacex.shared.data.HttpClient
 import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.respond
-import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.plugins.RedirectResponseException
 import io.ktor.client.plugins.ServerResponseException
@@ -26,7 +25,7 @@ import org.junit.Test
  */
 class LaunchApiTest {
     @Test
-    fun successResponse() = runTest {
+    fun `Parse success launches response`() = runTest {
         val engine = MockEngine { _ ->
             respond(
                 content = ByteReadChannel(
@@ -86,8 +85,8 @@ class LaunchApiTest {
 
     @Test
     @Suppress("SwallowedException")
-    fun failureRedirectResponse() = runTest {
-        failureRequest(HttpStatusCode.UseProxy) {
+    fun `Parse failure 3XX request`() = runTest {
+        failureRequestTest(HttpStatusCode.UseProxy) {
             try {
                 getOrThrow()
                 false
@@ -100,8 +99,8 @@ class LaunchApiTest {
 
     @Test
     @Suppress("SwallowedException")
-    fun failureClientRequest() = runTest {
-        failureRequest(HttpStatusCode.BadRequest) {
+    fun `Parse failure 4XX request`() = runTest {
+        failureRequestTest(HttpStatusCode.BadRequest) {
             try {
                 getOrThrow()
                 false
@@ -114,8 +113,8 @@ class LaunchApiTest {
 
     @Test
     @Suppress("SwallowedException")
-    fun failureServerResponse() = runTest {
-        failureRequest(HttpStatusCode.InternalServerError) {
+    fun `Parse failure 5XX request`() = runTest {
+        failureRequestTest(HttpStatusCode.InternalServerError) {
             try {
                 getOrThrow()
                 false
@@ -127,15 +126,61 @@ class LaunchApiTest {
     }
 
     @Test
-    fun detailRequest() = runTest {
-        val api = LaunchesAPI(HttpClient("api.spacexdata.com", OkHttp.create(), object : Logger {
-            override fun log(message: String) {
-                println(message)
-            }
-        }))
-        val response = api.specificLaunch("633f72000531f07b4fdf59c2")
+    fun `Parse success detail launch response`() = runTest {
+        val api = launchesApi(MockEngine { _ ->
+            respond(
+                content = """
+                    {
+                        "docs": [
+                            {
+                                "fairings": {
+                                    "recovered": null
+                                },
+                                "links": {
+                                    "youtube_id": null,
+                                    "article": null
+                                },
+                                "rocket": {
+                                    "name": "Falcon 9",
+                                    "id": "5e9d0d95eda69973a809d1ec"
+                                },
+                                "success": null,
+                                "details": null,
+                                "launchpad": {
+                                    "full_name": "Cape Canaveral Space Force Station Space Launch Complex 40",
+                                    "id": "5e9e4501f509094ba4566f84"
+                                },
+                                "flight_number": 198,
+                                "name": "SES-18 & SES-19",
+                                "date_unix": 1667260800,
+                                "upcoming": true,
+                                "id": "633f72000531f07b4fdf59c2"
+                            }
+                        ],
+                        "totalDocs": 1,
+                        "offset": 0,
+                        "limit": 1,
+                        "totalPages": 1,
+                        "page": 1,
+                        "pagingCounter": 1,
+                        "hasPrevPage": false,
+                        "hasNextPage": false,
+                        "prevPage": null,
+                        "nextPage": null
+                    }
+                """.trimIndent(),
+                status = HttpStatusCode.OK,
+                headers = headersOf(HttpHeaders.ContentType, "application/json")
+            )
+        })
+        val id = "633f72000531f07b4fdf59c2"
+        val response = api.specificLaunch(id)
         println(response)
         assertTrue(response.isSuccess)
+        response.onSuccess {
+            assertEquals(id, it.id)
+        }
+
     }
 
     private fun launchesApi(engine: MockEngine) = LaunchesAPI(HttpClient("api.spacexdata.com", engine, object : Logger {
@@ -143,6 +188,20 @@ class LaunchApiTest {
             println(message)
         }
     }))
+
+    private suspend fun failureRequestTest(code: HttpStatusCode, check: Result<*>.() -> Boolean) {
+        val api = launchesApi(failureEngine(code))
+        val response = api.allLaunches(
+            query = ILaunchesFilterRepository.Filters(
+                false,
+                false,
+                setOf(),
+                "",
+            ), page = 1, pageSize = 2
+        )
+        assertTrue(response.isFailure)
+        assert(response.check())
+    }
 
     private fun failureEngine(code: HttpStatusCode) = MockEngine { _ ->
         respond(
@@ -152,19 +211,5 @@ class LaunchApiTest {
             status = code,
             headers = headersOf(HttpHeaders.ContentType, "application/json")
         )
-    }
-
-    private suspend fun failureRequest(code: HttpStatusCode, check: Result<*>.() -> Boolean) {
-        val api = launchesApi(failureEngine(code))
-        val response = api.allLaunches(
-            query = ILaunchesFilterRepository.Filters(
-                false,
-                false,
-                setOf(),
-                "",
-            ), page = 21, pageSize = 10
-        )
-        assertTrue(response.isFailure)
-        assert(response.check())
     }
 }
