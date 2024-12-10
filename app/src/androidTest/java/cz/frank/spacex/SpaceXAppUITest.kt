908701cc1892e9459e10f8e30f7f9391e33d00b0
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2024-12-10 13:56:00 +0100

    For ui tests unload application module and load module with fake implementations

--- app/src/androidTest/java/cz/frank/spacex/SpaceXAppUITest.kt
+++ app/src/androidTest/java/cz/frank/spacex/SpaceXAppUITest.kt
@@ -1,13 +1,66 @@
 package cz.frank.spacex
 
+import android.content.Context
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import cz.frank.spacex.crew.data.CrewRepository
+import cz.frank.spacex.crew.data.ICrewApi
+import cz.frank.spacex.crew.ui.search.CrewSearchViewModel
 import cz.frank.spacex.launches.data.RocketsDataSource
+import cz.frank.spacex.launches.data.database.dao.ILaunchesFilterDao
+import cz.frank.spacex.launches.data.database.dao.IRefreshDao
+import cz.frank.spacex.launches.data.database.dao.IRemoteKeyDao
+import cz.frank.spacex.launches.data.repository.*
+import cz.frank.spacex.launches.ui.filter.LaunchFilterViewModel
+import cz.frank.spacex.launches.ui.filter.rocket.LaunchFilterRocketViewModel
+import cz.frank.spacex.launches.ui.search.LaunchSearchViewModel
+import cz.frank.spacex.main.data.SpaceXDatabase
+import cz.frank.spacex.main.di.spaceXModule
 import cz.frank.spacex.main.ui.MainActivity
+import cz.frank.spacex.sharedtests.*
 import org.junit.Rule
 import org.junit.Test
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.unloadKoinModules
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
 
-class LaunchSearchEndToEndTest {
+class SpaceXAppUITest {
+    private val testModule = module {
+        single {
+            val context = ApplicationProvider.getApplicationContext<Context>()
+            Room.inMemoryDatabaseBuilder(context, SpaceXDatabase::class.java).build()
+        }
+        single { get<SpaceXDatabase>().launchesDao() }
+        singleOf(::FakeRemoteKeyDao) bind IRemoteKeyDao::class
+        singleOf(::FakeRefreshDao) bind IRefreshDao::class
+        singleOf(::FakeLaunchesFilterDao) bind ILaunchesFilterDao::class
+
+        singleOf(::FakeLaunchesRepository) bind ILaunchesRepository::class
+        singleOf(::LaunchesFilterRocketRepository) bind ILaunchesFilterRocketRepository::class
+        singleOf(::LaunchesFilterRepository) bind ILaunchesFilterRepository::class
+        factory { (filters: ILaunchesFilterRepository.Filters, pageSize: Int) ->
+            LaunchesMediator(get(), get(), get(), get(), get(), filters, pageSize)
+        }
+        viewModelOf(::LaunchSearchViewModel)
+        viewModelOf(::LaunchFilterViewModel)
+        viewModelOf(::LaunchFilterRocketViewModel)
+
+        singleOf(::FakeCrewApi) bind ICrewApi::class
+        singleOf(::CrewRepository)
+
+        viewModelOf(::CrewSearchViewModel)
+    }
+
+    init {
+        unloadKoinModules(spaceXModule)
+        loadKoinModules(testModule)
+    }
+
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
