472d13e314c21423393f8bc10bb2a71601f0c6d3
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2024-12-10 16:02:00 +0100

    Make launch detail request with IO dispatcher

--- app/src/main/java/cz/frank/spacex/launches/data/repository/LaunchesRepository.kt
+++ app/src/main/java/cz/frank/spacex/launches/data/repository/LaunchesRepository.kt
@@ -6,8 +6,10 @@ import cz.frank.spacex.launches.data.database.dao.LaunchDao
 import cz.frank.spacex.launches.data.database.entity.LaunchEntity
 import cz.frank.spacex.launches.domain.model.LaunchDetailModel
 import cz.frank.spacex.launches.domain.model.LaunchPreviewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.koin.core.parameter.parametersOf
@@ -30,7 +32,9 @@ class LaunchesRepository(
         items.map { it.toModel() }
     }
 
-    override suspend fun detailLaunch(id: String) = launchAPI.specificLaunch(id).mapCatching { it.toModel() }
+    override suspend fun detailLaunch(id: String) = withContext(Dispatchers.IO) {
+        launchAPI.specificLaunch(id).mapCatching { it.toModel() }
+    }
 }
 
 fun LaunchEntity.toModel() = LaunchPreviewModel(
