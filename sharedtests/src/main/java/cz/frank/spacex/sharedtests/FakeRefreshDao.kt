49c1a1e74b33584c41cf41ed8691e850fb8afbac
Matyáš Frank <167653683+matyas-frank@users.noreply.github.com>
2024-12-10 15:33:52 +0100

    Remake fake refresh dao to use state flow

--- sharedtests/src/main/java/cz/frank/spacex/sharedtests/FakeRefreshDao.kt
+++ sharedtests/src/main/java/cz/frank/spacex/sharedtests/FakeRefreshDao.kt
@@ -2,14 +2,13 @@ package cz.frank.spacex.sharedtests
 
 import cz.frank.spacex.launches.data.database.dao.IRefreshDao
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class FakeRefreshDao: IRefreshDao {
-    private var _timeOfLastRefreshInMillis: Long? = null
-
-    override val timeOfLastRefreshInMillis: Flow<Long?> = flow { _timeOfLastRefreshInMillis }
+    private var _timeOfLastRefreshInMillis = MutableStateFlow<Long?>(null)
+    override val timeOfLastRefreshInMillis: Flow<Long?> = _timeOfLastRefreshInMillis
 
     override suspend fun changeTimeOfLastUpdate(millis: Long?) {
-        _timeOfLastRefreshInMillis = millis
+        _timeOfLastRefreshInMillis.value = millis
     }
 }
